1967043b34a171cffad83887c08d0410cd0053d4
Sergey Zyablitsky <sergey.zyablitsky@gmail.com>
2013-07-29 05:59:32 +1100

    footer, page title

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,13 +2,12 @@ require 'spec_helper'
 
 describe ApplicationHelper do
   
-  #context '@page_title set' do
-  #  before(:each) { @page_title = '!' }
-  #  it { title.should == '! - ' }
-  #end
+  context 'page_title set' do
+    it { title('!').should == 'Sky Memories | !' }
+  end
 
-  #context '@page_title not set' do
-  #  it { title.should be_nil }
-  #end
+  context 'page_title not set' do
+    it { title('').should == 'Sky Memories' }
+  end
 
 end
