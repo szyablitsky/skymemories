2324f933cdc4544e1d2e6f1a95e488c4f2085006
Sergey Zyablitsky <sergey.zyablitsky@gmail.com>
2013-08-06 13:54:04 +1100

    posts scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130719103114) do
+ActiveRecord::Schema.define(version: 20130806013928) do
 
   create_table "movies", force: true do |t|
     t.integer  "vimeo_id"
@@ -20,6 +20,17 @@ ActiveRecord::Schema.define(version: 20130719103114) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.boolean  "published"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "posts", ["title"], name: "index_posts_on_title", unique: true
 
   create_table "users", force: true do |t|
     t.string   "name"
