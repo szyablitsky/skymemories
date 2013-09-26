8291638e610f488a4e25b3f9911b28174be7cd97
Sergey Zyablitsky <sergey.zyablitsky@gmail.com>
2013-09-26 12:54:29 +1100

    puma config

--- config/puma.rb
+++ config/puma.rb
@@ -8,4 +8,4 @@ state_path      '/home/finch/sites/sm.finch.pro/current/tmp/puma/state'
 stdout_redirect '/home/finch/logs/sm_finch_pro.stdout.log', '/home/finch/logs/sm_finch_pro.stderr.log'
 quiet
 threads         0, 16
-bind            'unix:///home/finch/tmp/sm_finch_pro.sock'
+bind            'unix:///home/finch/sites/sm.finch.pro/tmp/puma.sock'
