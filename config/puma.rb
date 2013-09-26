358b88ed6724d10bce7960f4b6373d61b306de6a
Sergey Zyablitsky <sergey.zyablitsky@gmail.com>
2013-09-26 13:00:43 +1100

    puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/env puma
 
-directory       '/home/finch/sites/sm.finch.pro/current'
+#directory       '/home/finch/sites/sm.finch.pro/current'
 environment     'production'
 daemonize
-pidfile         '/home/finch/sites/sm.finch.pro/current/tmp/puma/pid'
-state_path      '/home/finch/sites/sm.finch.pro/current/tmp/puma/state'
-stdout_redirect '/home/finch/logs/sm_finch_pro.stdout.log', '/home/finch/logs/sm_finch_pro.stderr.log'
+#pidfile         '/home/finch/sites/sm.finch.pro/current/tmp/puma/pid'
+#state_path      '/home/finch/sites/sm.finch.pro/current/tmp/puma/state'
+stdout_redirect 'log/sm_finch_pro.stdout.log', 'log/sm_finch_pro.stderr.log'
 quiet
 threads         0, 16
-bind            'unix:///home/finch/sites/sm.finch.pro/tmp/puma.sock'
+bind            'unix:///home/finch/tmp/sm_finch_pro.sock'
