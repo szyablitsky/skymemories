b3fdccb0f3598d73e5ca2e3d320383209659b66c
Sergey Zyablitsky <sergey.zyablitsky@gmail.com>
2013-10-23 10:28:54 +1100

    new relic agent fix

--- config/puma.rb
+++ config/puma.rb
@@ -3,6 +3,7 @@
 quiet
 daemonize
 threads         0, 16
+workers         1
 environment     'production'
 bind            'unix:///home/finch/tmp/sm_finch_pro.sock'
 stdout_redirect 'log/sm_finch_pro.stdout.log', 'log/sm_finch_pro.stderr.log'
