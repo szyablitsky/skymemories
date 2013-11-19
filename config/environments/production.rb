67e2801135feb60ebf027f83b1c78530137974ab
Sergey Zyablitsky <sergey.zyablitsky@gmail.com>
2013-11-19 16:27:16 +1100

    fancybox assets in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,8 @@ Skymemories::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( bx_loader.gif controls.png )
+  # config.assets.precompile += %w( bx_loader.gif controls.png )
+  config.assets.precompile += %w( blank.gif fancybox* )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
