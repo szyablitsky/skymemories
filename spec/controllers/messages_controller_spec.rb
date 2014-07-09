5d57242a16286494cd92c7af69edc77494b1a293
Sergey Zyablitsky <sergey.zyablitsky@gmail.com>
2014-07-09 12:25:32 +1100

    gems update + test fix

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 RSpec.describe MessagesController, type: :controller do
 
   let(:valid_attributes) { FactoryGirl.attributes_for :message }
-  let(:valid_session) { {} }
+  # let(:valid_session) { {} }
 
   describe 'POST create' do
-    it 'sould redirect to contact page' do
-      post :create, {message: valid_attributes}, valid_session
-      expect(response).to redirect_to(contact_path)
+    it 'response status is success' do
+      post :create, message: valid_attributes, format: :js
+      expect(response.status).to eq 200
     end
   end
 
