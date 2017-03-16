4336b04c26e746f35d219ae75aa6593d8fcb94aa
Sergey Zyablitsky <sergey.zyablitsky@gmail.com>
2017-03-16 16:12:12 +1000

    mailer domain fix

--- app/mailers/message_notifier.rb
+++ app/mailers/message_notifier.rb
@@ -1,6 +1,6 @@
 class MessageNotifier < ActionMailer::Base
-  default from: 'Sky Memories <no-reply@skymemories.ru>',
-          to: 'info@skymemories.ru'
+  default from: 'Maevsky Pictires contact form <no-reply@skymemories.ru>',
+          to: 'max@maevskypictures.ru'
 
   def msg(msg)
     headers[:reply_to] = "#{msg['name']} <#{msg['email']}>"
